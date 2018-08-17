b00515a98d0c1b3a523131f6918ebb2d89c6b219
Tanya <tanyaselvog@gmail.com>
2018-08-17 11:59:08 -0700

    added JS so it functions & added readme notes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # catMealTracker
 Basic daily meal tracker for a pet
+
+Created using HTML, CSS & JS
+
+8.17.18 initial completion notes
+-finished and it works, but needs (future) revisions
+-would like to change functions so one function changes each individual row
+-would like to randomize background color of row
---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*JS Notes
+/element.addEventListener('event', functionName, [boolean]);*/
+/*
+function changingColor(){
+    //function changes the background color of each row when clicked on
+    document.body.style.backgroundColor = "red";
+}
+
+tr.addEventListener('click', changingColor, false);
+<button type="button" onclick="myFunction()">Set background color</button>
+
+*/
+function myBreakfast() {
+    document.getElementById("breakfast").style.backgroundColor = "#bb8fce ";
+}
+
+function myLunch() {
+    document.getElementById("lunch").style.backgroundColor = "#FFC300 ";
+}
+function myDinner() {
+    document.getElementById("dinner").style.backgroundColor = "#C70039 ";
+}
+function mySnack() {
+    document.getElementById("eveningSnack").style.backgroundColor = "#85c1e9 ";
+}
+//example from book on page 254
+
+/*
+function checkUsername(){
+    //code to check the length of the username
+}
+
+var element = document.getElementById('username');
+element.addEventListener('blur', checkUsername, false);
+*/
+
